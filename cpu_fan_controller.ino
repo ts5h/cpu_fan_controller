af7bea7171c6869c8eccf7c99dcbd2f1680aa149
at4 <at4@jc4.so-net.ne.jp>
2022-06-02 23:48:58 +0200

    Add functions

--- cpu_fan_controller.ino
+++ cpu_fan_controller.ino
@@ -1,8 +1,62 @@
-void setup() {
-  // put your setup code here, to run once:
+// CPU Fan Control from Pro micro
+// AMD Wraith Prism
 
+const unsigned int ANALOG_RATE_INPUT = A0;
+const unsigned int ANALOG_RATE_OUTPUT = A6;
+
+const unsigned int ANALOG_R_INPUT = A1;
+const unsigned int ANALOG_G_INPUT = A2;
+const unsigned int ANALOG_B_INPUT = A3;
+const unsigned int ANALOG_R_OUTPUT = A7;
+const unsigned int ANALOG_G_OUTPUT = A8;
+const unsigned int ANALOG_B_OUTPUT = A9;
+
+unsigned int rate = 0;
+unsigned int red = 0;
+unsigned int green = 0;
+unsigned int blue = 0;
+
+unsigned int averageNum = 10;
+unsigned int averageCycle = 30;
+
+void setup() {
+  pinMode(ANALOG_RATE_INPUT, INPUT);
+  pinMode(ANALOG_RATE_OUTPUT, OUTPUT);
+
+  pinMode(ANALOG_R_INPUT, INPUT);
+  pinMode(ANALOG_G_INPUT, INPUT);
+  pinMode(ANALOG_B_INPUT, INPUT);
+  pinMode(ANALOG_R_OUTPUT, OUTPUT);
+  pinMode(ANALOG_G_OUTPUT, OUTPUT);
+  pinMode(ANALOG_B_OUTPUT, OUTPUT);
+
+  Serial.begin(9600);
 }
 
 void loop() {
-  
+  rate = 0;
+  red = 0;
+  green = 0;
+  blue = 0;
+
+  for (int i = 0; i < averageNum; i++) {
+    rate += analogRead(ANALOG_RATE_INPUT);
+    red += analogRead(ANALOG_R_INPUT);
+    green += analogRead(ANALOG_G_INPUT);
+    blue += analogRead(ANALOG_B_INPUT);
+
+    delay(averageCycle);
+  }
+
+  rate = rate / averageNum / 4;
+  red = red / averageNum / 4;
+  green = green / averageNum / 4;
+  blue = blue / averageNum / 4;
+
+  Serial.println(rate);
+
+  analogWrite(ANALOG_RATE_OUTPUT, rate);
+  analogWrite(ANALOG_R_OUTPUT, red);
+  analogWrite(ANALOG_G_OUTPUT, green);
+  analogWrite(ANALOG_B_OUTPUT, blue);
 }
